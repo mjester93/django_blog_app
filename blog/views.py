cd6b26190dacc72ae5ed074ef1883369a03e475e
mjester93 <m.jester.93@gmail.com>
2020-09-19 10:49:13 -0400

    adding posts from database

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Michael Jester',
-        'title': 'Writing My First Django Application',
-        'content': 'first post content',
-        'date_posted': 'September 19, 2020'
-    },
-    {
-        'author': 'John Smith',
-        'title': 'Post #2',
-        'content': 'Here is my second post!',
-        'date_posted': 'September 20, 2020'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
 
     return render(request, 'blog/home.html', context)
