2889b2542cf75d9f15055eb04cdb4d38a374d46f
mjester93 <m.jester.93@gmail.com>
2020-09-25 21:01:40 -0400

    spelling error

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get['DEBUG_VALUE'] == 'True')
+DEBUG = (os.environ['DEBUG_VALUE'] == 'True')
 
 ALLOWED_HOSTS = ['moosesblog.herokuapp.com']
 
